98269cdda6cae8d05641d1f09704f8586d686d22
Michael Malinowski <mike.m7@hotmail.com>
2020-04-24 20:35:52 -0400

    Change directory names.

--- src/socketio.ino
+++ src/socketio.ino
@@ -3,8 +3,8 @@
 #include <SPI.h>
 #include "ArduinoSocketIO.h"
 
-char ssid[] = "Amigo";        // your network SSID (name)
-char pass[] = "ginatony";    // your network password (use for WPA, or use as key for WEP)
+char ssid[] = "";        // your network SSID (name)
+char pass[] = "";    // your network password (use for WPA, or use as key for WEP)
 int keyIndex = 0;            // your network key Index number (needed only for WEP)
 
 int status = WL_IDLE_STATUS;
